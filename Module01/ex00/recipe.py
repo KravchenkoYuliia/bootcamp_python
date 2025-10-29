7828cfcd3c18db1648969d38958959cc55224a5e
KravchenkoYuliia <yuliiakravchenko10@gmail.com>
2025-10-29 16:01:46 +0100

    feat: implement class Recipe and his methods

--- Module01/ex00/recipe.py
+++ Module01/ex00/recipe.py
@@ -1,9 +1,114 @@
 
 class Recipe:
-    def __init__(self):
-        self.name = ""
-        self.cooking_lvl = 0
-        self.cooking_time = 0
-        self.ingredients = ""
-        self.description = ""
-        self.recipe_type = ""
+    
+    def __init__(self, name, lvl, time, ingredients, description, recipe_type):
+        self.name = name
+        self.cooking_lvl = lvl
+        self.cooking_time = time
+        self.ingredients = ingredients
+        self.description = description
+        self.recipe_type = recipe_type
+
+    def __check_recipe__(self):
+        try:
+            self._check_name_()
+            self._check_lvl_()
+            self._check_cooking_time_()
+            self._check_ingredients_()
+            self._check_description_()
+            self._check_recipe_type_()
+        except:
+            raise
+    
+    def __str__(self):
+        recipe = f"The recipe of {self.name}:) \n\nCooking level = {self.cooking_lvl}/5\nEstimated time of cooking = {self.cooking_time} minutes\nAll you need is: {', '.join(elem for elem in self.ingredients)}"
+        if self.description != "":
+            recipe += f"\nAbout {self.name}: {self.description}"
+        recipe += f"\nType: {self.recipe_type}"
+
+        return recipe
+
+    def __check_string__(self, data_to_check):
+        if (str(data_to_check,).isdigit()
+            or str(data_to_check,) == ""
+            or str(data_to_check,).isspace()
+            or not str(data_to_check,).strip().isprintable()
+            ):
+
+            raise
+
+
+    def _check_name_(self):
+        try:
+            str(self.name)
+            self.__check_string__(self.name)
+        except:
+            print("Invalid name for the recipe")
+            raise Exception("Invalid name for the recipe")
+
+
+
+    def _check_lvl_(self):
+         try:
+             int(self.cooking_lvl)
+         except:
+            print("Invalid cooking level")
+            raise Exception("Invalid cooking level")
+        
+         if int(self.cooking_lvl) < 1 or int(self.cooking_lvl) > 5:
+            print("Invalid cooking level")
+            raise Exception("Invalid cooking level")
+            
+    
+    def _check_cooking_time_(self):
+        try:
+            int(self.cooking_time)
+        except:
+            print("Invalid cooking time")
+            raise Exception("Invalid cooking time")
+        if int(self.cooking_time) < 1 or int(self.cooking_time) > 1000:
+            print("Invalid cooking time")
+            raise Exception("Invalid cooking time")
+    
+    def _check_ingredients_(self):
+        
+        for elem in self.ingredients:
+            try:
+                self.__check_string__(elem)
+                str(elem)
+            except:
+                print("Invalid ingredients")
+                raise Exception("Invalid ingredients")
+    
+
+    def _check_description_(self):
+        try:
+            str(self.description)
+        except:
+            print("Invalid description")
+            raise Exception("Invalid description")
+           
+        if (str(self.description).isdigit()
+            or str(self.description).isspace()
+            or not str(self.description).strip().isprintable()
+            ):
+
+            print("Invalid description")
+            raise Exception("Invalid description")
+    
+
+    def _check_recipe_type_(self):
+        try:
+            str(self.recipe_type)
+        except:
+            print("Invalid recipe type\nPut [starter], [lunch] or [dessert]")
+            raise Exception("Invalid recipe type")
+        
+        if (self.recipe_type != "starter"
+                and self.recipe_type != "lunch"
+                and self.recipe_type != "dessert"
+                ):
+
+            print("Invalid recipe type\nPut [starter], [lunch] or [dessert]")
+            raise Exception("Invalid recipe type")
+
--- Module01/ex00/test.py
+++ Module01/ex00/test.py
@@ -1,31 +1,14 @@
 import recipe
 
-def check_recipe(Pancake):
-    if Pancake.name == "" or Pancake.name != str:
-        print("Error in name\nPut correct name for the recipe")
-        return
-    if Pancake.cooking_lvl < 0 or Pancake.cooking_lvl > 5 or Pancake.cooking_lvl.isdigit() == False:
-        print("Error in cooking_lvl\nPut level from 0 to 5")
-        return
-    if Pancake.cooking_time < 0 or Pancake.cooking_time.isdigit() == False:
-        print("Error in cooking_time")
-
 def main():
-    Pancake = recipe.Recipe()
-    Pancake.name = "Pancake"
-    Pancake.cooking_lvl = 3
-    Pancake.cooking_time = 20
-    Pancake.ingredients = {"milk", "eggs", "flour"}
-    Pancake.description = "Put it together on the frying pan"
-    Pancake.recipe_type = "dessert"
+    
+    Pancake = recipe.Recipe("Pancake", 3, 20, ["milk", "eggs"], "So tasty", "dessert")
+    try:
+        Pancake.__check_recipe__()
+    except:
+        return
 
-    status = check_recipe(Pancake)
-
-    Soup = recipe.Recipe()
-    Soup.name = "Soup"
-
-    print(Pancake.name)
-    print(Soup.name)
+    print(str(Pancake))
 
 if __name__=="__main__":
     main()
